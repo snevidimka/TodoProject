1466293e34efe34bdfd197ce5e6fdf0644c458e1
snevidimka <snevidimka@yandex.ru>
2020-04-15 23:58:34 +0300

    Final less2

--- main/views.py
+++ main/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+data = {
+    'lists': [
+        {'name': 'Работа', 'is_done': True},
+        {'name': 'Дом', 'is_done': False},
+        {'name': 'Учеба', 'is_done': True}
+    ],
+    'user_name': 'Admin',
+}
+
+
 def main_view(request):
     '''при запросе вернет ответ со страничкой index.html'''
-    return render(request, 'index.html')
+    context = data
+    return render(request, 'index.html', context)
