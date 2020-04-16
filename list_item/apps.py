2eb6524b29b7e5c6f9657f308d52e6319a28f0e4
snevidimka <snevidimka@yandex.ru>
2020-04-16 03:14:36 +0300

    Homework less2

---
+++ list_item/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ListItemConfig(AppConfig):
+    name = 'list_item'
---
+++ list_item/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+data = {
+    'events': [
+        {'name': 'Купить', 'is_done': True, 'date' : '29.12.2020'},
+        {'name': 'Заказать', 'is_done': False, 'date' : '30.12.2020'},
+        {'name': 'Разослать', 'is_done': False, 'date' : '31.12.2020'}
+    ],
+    'user_name': 'Admin',
+}
+def list_item_view(request):
+    '''при запросе вернет ответ со страничкой list.html'''
+    context = data
+    return render(request, 'list.html', context)
