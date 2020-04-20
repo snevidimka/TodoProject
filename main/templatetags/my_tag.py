0e05ec89876a5187e4e2fc4494dc4e30e8a8182c
snevidimka <snevidimka@yandex.ru>
2020-04-20 16:00:21 +0300

    less2 Homework *

--- main/templatetags/my_tag.py
+++ main/templatetags/my_tag.py
@@ -7,7 +7,6 @@ register = template.Library()
 @register.filter()
 def my_tag(value):
     result = ''
-    count = 6 - len(value)
-    for i in range(count):
+    for i in range(6 - len(value)):
         result += '<div class="table-data__table-row"></div>'
     return format_html(result)
