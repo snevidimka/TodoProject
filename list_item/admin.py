7122f477f26fcc4d1f8999e974f4bade73939000
snevidimka <snevidimka@yandex.ru>
2020-04-22 15:23:12 +0300

    less3

--- list_item/admin.py
+++ list_item/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from main.models import ListItemModel
+#
+#
+# class ListAdmin(admin.ModelAdmin):
+#
+#     list_display = ['id', 'created', 'name', 'is_done', 'listmodel_id']
+#     list_filter = ['id', 'created', 'name', 'is_done', 'expare_date']
+#     search_fields = ['name', 'listmodel_id']
+#
+#
+# admin.site.register(ListItemModel, ListAdmin)
