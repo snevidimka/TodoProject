f41ba2a85f0ecc52c995e09b3ffda74b02339b34
snevidimka <snevidimka@yandex.ru>
2020-05-24 13:42:50 +0300

    less10 Homework

--- registration/tests/functional_test.py
+++ registration/tests/functional_test.py
@@ -35,17 +35,17 @@ def test_create_view(live_server, new_client):
 
     button = browser.find_element_by_id('login-submit-btn')
     button.click()
-    time.sleep(2)
+    time.sleep(1)
 
     button = browser.find_element_by_class_name('add_new_list')
     button.click()
     element = browser.find_element_by_id('id_name')
     element.send_keys('Тестовый список')
-    time.sleep(2)
+    time.sleep(1)
 
     button = browser.find_element_by_id('login-submit-btn')
     button.click()
-    time.sleep(2)
+    time.sleep(1)
 
     assert browser.title == 'Главная'
     browser.close()
