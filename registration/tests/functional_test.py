859b22032a69604c3e2885987621119911dc13ae
snevidimka <snevidimka@yandex.ru>
2020-05-24 11:00:41 +0300

    less10 Homework

--- registration/tests/functional_test.py
+++ registration/tests/functional_test.py
@@ -16,7 +16,7 @@ TEST_CLIENT = {
 }
 
 
-def test_open_login_page(live_server, new_client):
+def test_create_view(live_server, new_client):
     """
     Новый пользователь Юля, открывает
     браузер и решает ввести 'http://127.0.0.1:8000/'.
@@ -33,8 +33,19 @@ def test_open_login_page(live_server, new_client):
 
     button = browser.find_element_by_id('login-submit-btn')
     button.click()
+    time.sleep(2)
 
-    # WebDriverWait(browser, 3).until(es.title_is('Главная'))
-    time.sleep(5)
+    button = browser.find_element_by_class_name('add_new_list')
+    button.click()
+    element = browser.find_element_by_id('id_name')
+    element.send_keys('Тестовый список')
+    time.sleep(2)
+
+    button = browser.find_element_by_id('login-submit-btn')
+    button.click()
+    time.sleep(2)
+
     assert browser.title == 'Главная'
     # browser.close()
+
+
